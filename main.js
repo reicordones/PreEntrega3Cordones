4a6cadd31e49efa4725599a66b0c1ab841b37d73
reicordones <cordonesrj@gmail.com>
2023-05-01 23:34:25 -0500

    calculador de masa corporal funcionando

---
+++ main.js
@@ -0,0 +1,34 @@
+let nombre = prompt("Ingrese Nombre");
+let nacimiento = parseInt(prompt("Ingrese su año de nacimiento"));
+
+function indiceMasaCorporal(peso, estatura){
+    return peso / (estatura * estatura)
+}
+
+let edad = 2023 - nacimiento;
+alert(`Hola ${nombre} tienes ${edad} años de edad`);
+
+if(edad >= 35){
+    alert("Analizaremos tu riesgo cardiovascular")
+    let peso = prompt("Ingresa tu peso en Kg")
+    let estatura = prompt("Ingresa tu estatura en metros")
+    let resultado = indiceMasaCorporal(peso, estatura);
+    
+    switch (true){
+        case resultado < 18.5:
+            alert(`${nombre} tu indice de masa corporal es ${resultado}, estás bajo de peso`);
+            break;
+        case resultado >= 18.5 && resultado < 25:
+            alert(`${nombre} tu indice de masa corporal es ${resultado}, tu peso es normal`);
+            break;
+        case resultado >= 25 && resultado < 30:
+            alert(`${nombre} tu indice de masa corporal es ${resultado}, estás por encima de un peso normal`);
+            break;
+        default:
+            alert(`${nombre} tu indice de masa corporal es ${resultado}, tienes sobrepeso, debes bajar de peso por tu salud`);
+            break;
+    }
+}else{
+    alert("No te encuentras en edad de riesgo cardiovascular")
+}
+
