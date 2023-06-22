0b7e628cfcd158009deb84af20e30ba542dfddb8
reicordones <cordonesrj@gmail.com>
2023-06-21 23:28:52 -0500

    creación de un archivo .json y uso del fetch

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Definición de la clase Paciente
+
 class Paciente {
   constructor(nombre, apellido, dni, peso, altura) {
     this.nombre = nombre;
@@ -7,30 +9,38 @@ class Paciente {
     this.altura = altura;
   }
 
+ // Método para calcular el IMC del paciente
+
   calcularIMC() {
     let resultado = this.peso / (this.altura ** 2);
     return resultado.toFixed(2);
   }
+
+// Método para obtener la recomendación según el IMC del paciente
 
   obtenerRecomendacion() {
     let imc = this.calcularIMC();
     let recomendacion = '';
 
     if (imc < 18.5) {
-      recomendacion = 'Estás bajo de peso. Consulta a un profesional de la salud.';
+      recomendacion = 'Estás bajo de peso. Consulta a un profesional de la salud';
     } else if (imc >= 18.5 && imc < 25) {
       recomendacion = 'Tu peso es normal. ¡Sigue así!';
     } else if (imc >= 25 && imc < 30) {
-      recomendacion = 'Estás por encima de un peso normal. Considera realizar actividad física y llevar una dieta balanceada.';
+      recomendacion = 'Estás por encima de un peso normal. Considera realizar actividad física y llevar una dieta balanceada';
     } else {
-      recomendacion = 'Tienes sobrepeso. Es importante que consultes a un profesional de la salud y adoptes hábitos saludables.';
+      recomendacion = 'Tienes sobrepeso. Es importante que consultes a un profesional de la salud y adoptes hábitos saludables';
     }
 
     return recomendacion;
   }
 }
 
+// Array para almacenar los pacientes
+
 let arrayPacientes = [];
+
+// Cargar datos de pacientes desde localStorage o archivo JSON
 
 if (localStorage.getItem('pacientes')) {
   const pacientesData = JSON.parse(localStorage.getItem('pacientes'));
@@ -45,16 +55,41 @@ if (localStorage.getItem('pacientes')) {
     );
     arrayPacientes.push(paciente);
   });
+} else {
+  fetch('pacientes.json')
+    .then(response => response.json())
+    .then(data => {
+      arrayPacientes = data.map(pacienteData => new Paciente(
+        pacienteData.nombre,
+        pacienteData.apellido,
+        pacienteData.dni,
+        pacienteData.peso,
+        pacienteData.altura
+      ));
+
+      localStorage.setItem('pacientes', JSON.stringify(arrayPacientes));
+
+      mostrarPacientes();
+    })
+    .catch(error => {
+      console.error('Error al cargar los datos de pacientes:', error);
+    });
 }
+
+// Obtener referencias a elementos del DOM
 
 const formulario = document.getElementById('formulario');
 const btnRegistrar = document.getElementById('btnRegistrar');
 const btnCalcularIMC = document.getElementById('btnCalcularIMC');
 const infoPacientes = document.getElementById('infoPacientes');
 
+// Event listeners
+
 formulario.addEventListener('submit', handleSubmit);
 btnCalcularIMC.addEventListener('click', handleCalcularIMC);
 btnLimpiar.addEventListener('click', limpiarPantalla);
+
+// Función para manejar el envío del formulario de registro de paciente
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -81,23 +116,32 @@ function handleSubmit(e) {
   mostrarPacientes();
 }
 
+// Función para manejar el botón de cálculo de IMC
+
 function handleCalcularIMC() {
   const dni = prompt('Ingrese el DNI del paciente:');
   mostrarIMCPaciente(dni);
 }
 
+// Función para mostrar la información de los pacientes en el DOM
+
 function mostrarPacientes() {
-  console.log(arrayPacientes); // Imprimir por consola
+  console.log(arrayPacientes);
 
   // let pacientesHTML = '';
 
   // arrayPacientes.forEach(function (paciente) {
+  //   const imc = paciente.calcularIMC();
+  //   const recomendacion = paciente.obtenerRecomendacion();
+
   //   const pacienteHTML = `
   //     <div>
   //       <h3>${paciente.nombre} ${paciente.apellido}</h3>
   //       <p>DNI: ${paciente.dni}</p>
   //       <p>Peso: ${paciente.peso} kg</p>
   //       <p>Altura: ${paciente.altura} m</p>
+  //       <p>IMC: ${imc}</p>
+  //       <p>Recomendación: ${recomendacion}</p>
   //       <hr>
   //     </div>
   //   `;
@@ -108,9 +152,13 @@ function mostrarPacientes() {
   // infoPacientes.innerHTML = pacientesHTML;
 }
 
+// Función para limpiar la pantalla de información de pacientes
+
 function limpiarPantalla() {
   infoPacientes.innerHTML = '';
 }
+
+// Función para mostrar el IMC y recomendación de un paciente específico
 
 function mostrarIMCPaciente(dni) {
   const pacienteIndex = arrayPacientes.findIndex(function (paciente) {
